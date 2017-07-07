de5b7920210b89079fb8efe1000500c5330c1a8b
Jeff <jtakaki@murad.com>
2017-07-07 10:01:07 -0700

    change port number

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = require('./config');
 var setupController = require('./controllers/setupController');
 var apiController = require('./controllers/apiController');
 
-var port = process.env.PORT || 3000;
+app.set('port', (process.env.PORT || 5000));
 
 app.use('/', express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
